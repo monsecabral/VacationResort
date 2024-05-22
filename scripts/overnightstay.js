b513200e55a79830f4b89fab51f9462a512f0fc3
monsecabral <cabral_monse@yahoo.com>
2024-05-22 16:03:31 -0400

    checkpoint from last week

--- scripts/overnightstay.js
+++ scripts/overnightstay.js
@@ -1,1 +1,20 @@
 "use strict";
+
+
+// text values
+const checkInDate = document.getElementById("checkInDate");
+
+const numOfNights = document.getElementById("numOfNights");
+
+const numOfAdults = document.getElementById("numOfAdults");
+const numOfChildren = document.getElementById("numOfChildren");
+
+
+// buttons
+const queenRadio = document.getElementById("queenRadio");
+const kingRadio = document.getElementById("kingRadio");
+const twoBedRadio = document.getElementById("twoBedRadio");
+
+const noneRadio = document.getElementById("noneRadio");
+const seniorRadio = document.getElementById("seniorRadio");
+const militaryRadio = document.getElementById("militaryRadio");
